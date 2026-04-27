84198095d6b63ef8f430c861dc4adcf9da9cfcb2
danilojduarte <76825919+danilojduarte@users.noreply.github.com>
2026-04-27 14:37:26 -0300

    Criando novas section, noticias

---
+++ js/noticias.js
@@ -0,0 +1,18 @@
+[
+  {
+    "id": 1,
+    "titulo": "Festa na Praça do Bairro",
+    "data": "25 Abr, 2026",
+    "resumo": "Neste sábado teremos feira de artesanato e música ao vivo para toda a família.",
+    "imagem": "img/noticias/festa-praca.jpg",
+    "link": "#"
+  },
+  {
+    "id": 2,
+    "titulo": "Nova Ciclovia na Avenida Principal",
+    "data": "20 Abr, 2026",
+    "resumo": "As obras começam na próxima segunda-feira e prometem melhorar a mobilidade.",
+    "imagem": "img/noticias/ciclovia.jpg",
+    "link": "#"
+  }
+]
